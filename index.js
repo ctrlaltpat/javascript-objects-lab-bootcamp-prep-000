23cfcc1a9fe5bc2a51bc72389874786f0871635c
patrickp246@gmail.com <patrickp246@gmail.com>
2018-08-08 12:02:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,3 +9,11 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   object[key] = value;
   return object;
 }
+
+function deleteFromObjectByKey(object, key) {
+  var newObj = Object.assign({}, object);
+  
+  delete newObj.key;
+  
+  return newObj;
+}
