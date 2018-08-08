bb80fe25013e6ce147765c809f9b2cdc00615600
patrickp246@gmail.com <patrickp246@gmail.com>
2018-08-08 12:00:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,4 +5,7 @@ function updateObjectWithKeyAndValue(object, key, value) {
   return newObj;
 }
 
-function 
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object.key = value;
+  return object;
+}
